64cf98c79046e575e60dca67fb1d49b7b8cd992f
zhengyf <ceil_124@163.com>
2024-11-12 03:55:14 +0800

    feat: 调整默认值

--- mode/ccm/ccm.go
+++ mode/ccm/ccm.go
@@ -14,7 +14,7 @@ const (
 	ccmBlockSize         = 16
 	ccmTagSize           = 16
 	ccmMinimumTagSize    = 4
-	ccmStandardNonceSize = 13
+	ccmStandardNonceSize = 13 // 这里最好改成可修改，或者跟随iv判断
 )
 
 var errOpen = errors.New("cipher: message authentication failed")
